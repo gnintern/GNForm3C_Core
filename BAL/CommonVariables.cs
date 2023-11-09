a464900d81da6e0f833be3928e139986e4d5fc4c
uttamdhokiya2003 <uttamdhokiya84@gmail.com>
2023-11-09 13:14:10 +0530

    Fix Common Logos

---
+++ BAL/CommonVariables.cs
@@ -0,0 +1,12 @@
+﻿namespace GNForm3C_.BAL
+{
+
+    public static class CommonVariables
+    {
+        #region Common Images
+        public static string SmallLogo = "/Theme/assets/media/logos/GNWebsoft_OnlyLogo.png";
+        public static string SlidebarImage = "/Theme/assets/media/logos/GNWebsoft_Logo.png";
+        #endregion
+    }
+
+}
